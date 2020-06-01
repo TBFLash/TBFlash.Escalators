46c22a78a05407a3d54e0ae73921d92dbbcbb9d6
Jason White <jason.white10@gmail.com>
2020-05-31 21:19:59 -0400

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimAirport.Modding.Base;
+using SimAirport.Modding.Settings;
+
+namespace TBFlash.LongEscalator
+{
+    public class Mod : BaseMod
+    {
+        public override string Name => "TBFlash.LongEscalator";
+
+        public override string InternalName => "TBFlash.LongEscalator";
+
+        public override string Description => "";
+
+        public override string Author => "TBFlash";
+
+        public override SettingManager SettingManager { get; set; }
+
+        public override void OnTick()
+        {       
+        }
+    }
+}
